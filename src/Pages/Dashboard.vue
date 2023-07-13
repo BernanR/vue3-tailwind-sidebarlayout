6aca65d019f0ad48564beb60bc212e8fa998a75c
Bernan Ribeiro <bernan.ribeiro@itwsgroup.com>
2023-07-13 18:30:45 -0300

    🚀 fixing route url and put github link

--- src/Pages/Dashboard.vue
+++ src/Pages/Dashboard.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
           <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />Read more <span aria-hidden="true">&rarr;</span></a>
+            Donwload this template for free on. <a href="https://github.com/BernanR/vue3-tailwind-sidebarlayout" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />Git Hub <span aria-hidden="true">&rarr;</span></a>
           </div>
         </div>
         <div class="text-center">
